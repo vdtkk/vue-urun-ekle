6e746864aa5e543db270a958e3b6ec9adb533559
unknown <vdtkk@hotmail.com>
2023-11-28 19:41:20 +0300

    urun ekleme uygulaması

---
+++ src/components/Product.vue
@@ -0,0 +1,12 @@
+<template>
+         <div class="col-md-2 card">
+                    <slot></slot>
+
+                </div>
+</template>
+<script>
+
+
+
+
+</script>
---
+++ src/components/Products.vue
@@ -0,0 +1,54 @@
+<template>
+        <div class="row product-container">
+        <app-product v-for="product in productList">
+
+            <img class="card-img-top" src="/src/assets/default.png" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <small>
+                                <strong>Adet : </strong> 1 </small>
+                            <br>
+                            <small>
+                                <strong>Fiyat : </strong> 10 </small>
+                            <br>
+                            <small>
+                                <strong>Tutar : </strong> 10 </small>
+                        </div>
+
+        </app-product>
+
+        </div>
+</template>
+<script>
+    import { eventBus } from "../main";
+    import Product from "./Product.vue"
+
+export default{
+ 
+
+    components: {
+
+        appProduct:Product
+
+    },
+    data(){
+
+        return{
+
+            productList: [],
+        }
+    },
+    create(){
+        //eventBus child'den childe taşınmadıında kullanılır. 
+        
+        eventBus.$on("productAdded",(product)=>{
+            this.productList.push(product);
+
+        });
+
+    }
+
+  }
+
+ 
+</script>
